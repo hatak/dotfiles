d44c152c00d9ef0185264b3d78708e0614b5b161
Hisashi HATAKEYAMA <id.hatak@gmail.com>
2011-04-13 02:28:26 +0900

    added initialize script

---
+++ scripts/initialize.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# get submodule
+git submodule init
+git submodule update
+git submodule foreach 'git checkout master'
+
+# create symbolic link
+ln -sf `pwd`/.gitconfig ~/.gitconfig
+ln -sf `pwd`/.my.cnf ~/.my.cnf
+ln -sf `pwd`/.screenrc ~/.screenrc
+ln -sf `pwd`/.vim ~/.vim
+ln -sf `pwd`/.vimrc ~/.vimrc
+ln -sf `pwd`/.zshrc ~/.zshrc
