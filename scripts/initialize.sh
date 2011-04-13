fe84afa7018aa227391a571f6a4ff402f34a4b92
Hisashi HATAKEYAMA <id.hatak@gmail.com>
2011-04-13 17:07:44 +0900

    fixes initialize script

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -6,9 +6,9 @@ git submodule update
 git submodule foreach 'git checkout master'
 
 # create symbolic link
+ln -snf `pwd`/.vim ~/.vim
 ln -sf `pwd`/.gitconfig ~/.gitconfig
 ln -sf `pwd`/.my.cnf ~/.my.cnf
 ln -sf `pwd`/.screenrc ~/.screenrc
-ln -sf `pwd`/.vim ~/.vim
 ln -sf `pwd`/.vimrc ~/.vimrc
 ln -sf `pwd`/.zshrc ~/.zshrc
