93dcfa50eb1b23c9c7fb05ca77d7eaca56c3070c
Hisashi HATAKEYAMA <id.hatak@gmail.com>
2012-06-17 01:29:21 +0900

    fix initialize scripts
    * add .tmux.conf and .perltidyrc

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -12,3 +12,5 @@ ln -sf `pwd`/.my.cnf ~/.my.cnf
 ln -sf `pwd`/.screenrc ~/.screenrc
 ln -sf `pwd`/.vimrc ~/.vimrc
 ln -sf `pwd`/.zshrc ~/.zshrc
+ln -sf `pwd`/.tmux.conf ~/.tmux.conf
+ln -sf `pwd`/.perltidyrc ~/.perltidyrc
